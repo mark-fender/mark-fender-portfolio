1810db3f4912b1b7e21ccb2f60550229c3d29f1a
Marek Lipčák <marek.lipcak@avitech.sk>
2025-02-26 17:05:02 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/**/*.{js,ts,jsx,tsx}',
+    './src/hooks/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '640px',
+        md: '768px', 
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+  },
   plugins: [],
   darkMode: 'class',
 };
