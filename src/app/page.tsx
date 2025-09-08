03c21fbf50ce54b7767db22fb8d1383cbd4fc88b
Marek Lipčák <marek.lipcak@gmail.com>
2025-09-08 22:44:18 +0200

    fix: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,22 @@ import Skills from '@/components/Skills';
 import About from '@/components/About';
 import Experience from '@/components/experience/Experience';
 import Contact from '@/components/contact/Contact';
+import SectionDivider from '@/components/SectionDivider';
 
 const Home = () => {
   return (
     <main className='main'>
-        <Intro />
-        <About />
-        <Skills />
-        <Experience />
-        <ProjectList />
-        <Contact />
+      <Intro />
+      <SectionDivider />
+      <About />
+      <SectionDivider />
+      <Skills />
+      <SectionDivider />
+      <Experience />
+      <SectionDivider />
+      <ProjectList />
+      <SectionDivider />
+      <Contact />
     </main>
   );
 };
